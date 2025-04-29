26a696f5ea84ea794ed1980f9e8052676ce2bf47
Miguel Eduardo <migueleduardo0175@gmail.com>
2025-04-29 09:49:53 -0300

    Primeiro Comit

--- src/main/java/ConversorTemperaturaMiguelEduardo_2A.java
+++ src/main/java/ConversorTemperaturaMiguelEduardo_2A.java
@@ -1,11 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
-/**
- *
- * @author 2024105070084
- */
 public class ConversorTemperaturaMiguelEduardo_2A {
 
     public static void main(String[] args) {
